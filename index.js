202463102bc60d14846de0c81281b7ea676aa795
mighty-little-coder <144641610+mighty-little-coder@users.noreply.github.com>
2023-11-28 16:51:09 -0500

    havent pushed in a while. Added to index.js, shapes.js, as well as shapes.test.js

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
 const fs = require('fs');
 const inquirer = require('inquirer'); // common js
-const {Circle, Square, Triangle} = require("./lib/shapes");
+const { Circle, Square, Triangle } = require("./lib/shapes");
+
+// SVG Structuring class
+class SVGInput {
+  constructor() {
+    this.shapeInput = '';
+    this.textInput = '';
+  }
+  render() {
+    return `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+      ${this.shapeInput}
+      ${this.textInput}
+    </svg>`;
+  }
+
+  svgTextInput(text, color) {
+    this.textEl = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${text}</text>`;
+  }
+
+  svgShapeInput(shape) {
+    this.shapeInput = this.render();
+  }
+};
+
 
 // Writes questions to be answered in terminal
 inquirer
@@ -32,11 +55,20 @@ inquirer
     },
   ])
 
+  // Builds SVG from user input
   .then((svgBuilder) => {
     const filename = `logo.svg`;
-    const svg = 
-    ``
+    const svg =
+      `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+      ${SVGInput.svgTextInput}
+      ${SVGInput.svgShapeInput}
+  </svg>`
+
+    // Writes new SVG file
+    fs.writeFile(filename, svg, (err) =>
+      err ? console.log(err) : console.log('Successfully created a scalable logo in logo.svg file'))
   })
+
 
 
 
@@ -47,16 +79,16 @@ inquirer
 // I WANT to generate a simple logo for my projects
 // SO THAT I don't have to pay a graphic designer
 
-// Acceptance Criteria
-    // GIVEN a command-line application that accepts user input
-    // WHEN I am prompted for text
-    // THEN I can enter up to three characters
-    // WHEN I am prompted for the text color
-    // THEN I can enter a color keyword (OR a hexadecimal number)
-    // WHEN I am prompted for a shape
-    // THEN I am presented with a list of shapes to choose from: circle, triangle, and square
-    // WHEN I am prompted for the shape's color
-    // THEN I can enter a color keyword (OR a hexadecimal number)
+//// Acceptance Criteria
+//// GIVEN a command-line application that accepts user input
+//// WHEN I am prompted for text
+//// THEN I can enter up to three characters
+//// WHEN I am prompted for the text color
+//// THEN I can enter a color keyword (OR a hexadecimal number)
+//// WHEN I am prompted for a shape
+//// THEN I am presented with a list of shapes to choose from: circle, triangle, and square
+//// WHEN I am prompted for the shape's color
+//// THEN I can enter a color keyword (OR a hexadecimal number)
 // WHEN I have entered input for all the prompts
 // THEN an SVG file is created named `logo.svg`
 // AND the output text "Generated logo.svg" is printed in the command line
@@ -70,3 +102,11 @@ inquirer
 // const shape = new Triangle();
 // shape.setColor("blue");
 // expect(shape.render()).toEqual('<polygon points="150, 18 244, 182 56, 182" fill="blue" />');
+
+
+//<svg width="200" height="250" version="1.1" xmlns="http://www.w3.org/2000/svg">
+
+// <rect x="10" y="10" width="30" height="30" stroke="black" fill="transparent" stroke-width="5"/>
+// <rect x="60" y="10" rx="10" ry="10" width="30" height="30" stroke="black" fill="transparent" stroke-width="5"/>
+// <circle cx="25" cy="75" r="20" stroke="red" fill="transparent" stroke-width="5"/>
+// <polygon points="50 160 55 180 70 180 60 190 65 205 50 195 35 205 40 190 30 180 45 180" stroke="green" fill="transparent" stroke-width="5"/>
